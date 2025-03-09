1c84da48057d94b1d1b16ebbe17d2a833be61a79
Travis Gregory <tgregory@shutterstock.com>
2025-03-09 13:58:39 -0400

    scaffold out users and roles tabs

---
+++ client/src/components/RolesTab/RolesTab.tsx
@@ -0,0 +1,7 @@
+import { Tabs } from "@radix-ui/themes";
+
+const RolesTab = () => {
+  return <Tabs.Content value="roles">Roles</Tabs.Content>;
+};
+
+export default RolesTab;
---
+++ client/src/components/UsersTab/UsersTab.tsx
@@ -0,0 +1,11 @@
+import { Tabs } from "@radix-ui/themes";
+
+const UsersTab = () => {
+  return (
+    <Tabs.Content value="users">
+      Users Tab
+    </Tabs.Content>
+  );
+};
+
+export default UsersTab;
